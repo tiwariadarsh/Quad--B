f155736212de37bba2f99f1cd9f0b0cc42365217
Adarsh Tiwari <57039707+tiwariadarsh@users.noreply.github.com>
2021-09-21 00:33:31 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import '../style/SingleProduct.css';
 export default function About() {
     return (
         <div className="show-center">
-            <img className="show-photo" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/mv5bmge0ytixnmqtytmyms00zdjjltkwndctzjnhmte4yzjimjk4xkeyxkfqcgdeqxvymjgyoti4mgatat-v1-1589918053.jpg?crop=0.75xw:1xh;center,top&resize=480:*" alt="Adarsh" style={{height:"70vh",width:"50vh",marginLeft:"5rem"}} />
+            <img className="show-photo" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/mv5bmge0ytixnmqtytmyms00zdjjltkwndctzjnhmte4yzjimjk4xkeyxkfqcgdeqxvymjgyoti4mgatat-v1-1589918053.jpg?crop=0.75xw:1xh;center,top&resize=480:*" alt="Adarsh" style={{height:"65vh",width:"65%",marginLeft:"3.7rem",marginRight:"2rem"}}  />
             <section className="content-prod">
               <h2>Adarsh Tiwari</h2>
               <br/>
